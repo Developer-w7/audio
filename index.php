9888515a90eb05c42a8d3798a55439c4c4df33aa
anuroop <anuroopmohan007@hotmail.com>
2018-08-27 21:32:39 +0530

    9

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-exec("ffmpeg -i audio/nilamanal.mp3 -ab 64 audio/nilamanallow.mp3");
-
+// exec("ffmpeg -i /audio/nilamanal.mp3 -ab 64 /audio/nw.mp3");
+echo shell_exec("/usr/local/bin/ffmpeg -i test.mp3 -codec:a libmp3lame -b:a 128k out.mp3 2>&1");
 echo "haii anuroop";
 ?>
