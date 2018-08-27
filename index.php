a8ca875d040a5f71f2b23cac923d313404331e02
anuroop <anuroopmohan007@hotmail.com>
2018-08-27 21:52:17 +0530

    13

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
-exec('sudo /root/bin/ffmpeg -v',$output);
-print_r($ouput);
+// exec("ffmpeg -i /audio/nilamanal.mp3 -ab 64 /audio/nw.mp3");
+echo shell_exec(" /usr/local/bin/ffmpeg -i test.mp3 -codec:a libmp3lame -b:a 128k out.mp3 2>&1");
+
 ?>
